e80fb4af886b8aa57f65c285afffd9cf709920b9
Kevin <Kevin@kevinpc>
2019-12-10 11:09:15 -0500

    Adding support of IE10+
    No more usage of Object.assign (not supported by IE)
    
    Replace some variables names to get more comprehensive code.
    
    Adding some comments.

--- bgslider.js
+++ bgslider.js
@@ -1,72 +1,77 @@
-/**
-* Create a background slideshow 
-*/
+/*
+ * BgSlider by Kevin Bélanger
+ * https://github.com/kevin-belanger/bgSlider
+ * 
+ * Create a slideshow in the background of the page
+ * 
+ * Usage example: bgSlider(['img1.jpg', 'img2.jpg', 'img3.jpg']);
+ */
 function bgslider(imgList, options, css) {
 	
+	//Default values for parameters
+	imgList = imgList || [];
+	options = options || {};
+	css 	= css     || {};
+	
 	//Merge user options with default options
-	options = Object.assign({
-		TransitionDelay : 2000, 
-		SlideDelay 		: 5000,
-		appendTo 		: 'body',
-		className		: 'bgslider'
-	}, options);
+	options.TransitionDelay = options.TransitionDelay   || 3000, 
+	options.SlideDelay 	= options.SlideDelay        || 6000,
+	options.appendTo 	= options.appendTo          || 'body',
+	options.className	= options.className         ||'bgslider'
 	
-	//Merge user CSS with default CSS that will apply to img tags
-	css = Object.assign({
-		width : '100vw',
-		height : '100vh',
-		top : 0,
-		left: 0,
-		position: 'fixed',
-		zIndex: -10000,
-		backgroundSize: 'cover',
-		backgroundPosition: 'center center'
-	}, css );
+	//Merge user CSS with default CSS that will apply to div tags
+	css.width 		= css.width                 || '100vw',
+	css.height		= css.height                || '100vh',
+	css.top 		= css.top                   || 0,
+	css.left		= css.left                  || 0,
+	css.position            = css.position              || 'fixed',
+	css.zIndex 		= css.zIndex                || -10000,
+	css.backgroundSize 	= css.backgroundSize        || 'cover',
+	css.backgroundPosition 	= css.backgroundPosition    || 'center center'
 	
-	//Pointer that will be use to walk through imageList
-	var pointer = 0;
+	//Create first of two DIV tags that will be used to make fade transition
+	var div1 = document.createElement('DIV');
 	
-	//Create first of two IMG tags that will be used to make fade transition
-	var tag1 = document.createElement('DIV');
+	//Add className to DIV tag
+	div1.classList.add(options.className);
 	
-	//Add className to img tag
-	tag1.classList.add(options.className);
+	//Apply CSS to DIV tag
+	Object.keys(css).forEach(function(i){
+		div1.style[i] = css[i];
+	});
+	div1.style.transition = 'opacity '+ options.TransitionDelay/1000 + 's';
 	
-	//Apply CSS to img tag
-	Object.assign(tag1.style, css);
-	tag1.style.transition = 'opacity '+ options.TransitionDelay/1000 + 's';
+	//Second DIV tag is a clone of the 1st
+	var div2 = div1.cloneNode();
 	
-	//Second img tag is a clone of the 1st
-	tag2 = tag1.cloneNode();
-	
-	
-	function showNext() {
-		//initialisation of the pointer the first time the function is called
-		if (pointer == undefined)
-			pointer = 0;
+	var showNext = (function() {
+		//initialize pointer the first time the function is called
+		var pointer = 0;
 		
-		//if last image is reach, set pointer to first
-		if (pointer >= imgList.length)
-			pointer = 0;
-		
-		//Show next image
-		tag1.style.backgroundImage = 'url('+imgList[pointer]+')';
-		tag1.style.opacity=1;
-		tag2.style.opacity=0;
-		
-		//Swap e1 and e2 DOM reference
-		tag2 = [tag1, tag1 = tag2][0];
-		
-		//increment pointer to show next image next time
-		pointer++;
-	}
+		return function(){
+			//if last image is reach, set pointer to first
+			if (pointer >= imgList.length)
+				pointer = 0;
+			
+			//Show next image
+			div1.style.backgroundImage = 'url('+imgList[pointer]+')';
+			div1.style.opacity=1;
+			div2.style.opacity=0;
+			
+			//Swap e1 and e2 DOM reference
+			div2 = [div1, div1 = div2][0];
+			
+			//increment pointer to show next image next time
+			pointer++;
+		};
+	})();
 	
 
 	function start() {
 		//Insert img tags in DOM
 		var elem = document.querySelector(options.appendTo);
-		elem.insertBefore(tag1, elem.firstChild);
-		elem.insertBefore(tag2, elem.firstChild);
+		elem.insertBefore(div1, elem.firstChild);
+		elem.insertBefore(div2, elem.firstChild);
 		elem = undefined; delete(elem);
 		
 		//Start slideshow with interval
@@ -78,10 +83,6 @@ function bgslider(imgList, options, css) {
 	// in case the document is already rendered
 	if (document.readyState!='loading') start();
 	// otherwise, wait for document to be ready
-	else if (document.addEventListener) document.addEventListener('DOMContentLoaded', start);
-	// IE <= 8
-	else document.attachEvent('onreadystatechange', function(){
-		if (document.readyState=='complete') start();
-	});
+	else document.addEventListener('DOMContentLoaded', start);
 	
 }
